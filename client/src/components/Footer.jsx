400003fc7a4b485845c5dad6384a19be12d7983c
Prathamesh Lakhpati <prathmeshlakhpaty@gmail.com>
2023-07-25 22:53:16 +0530

    Created header and footer of the page

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./css/footer.css";
+
+function Footer() {
+  return <footer className="footer">&#169; 2023 UrbanBazaar</footer>;
+}
+
+export default Footer;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./css/header.css";
+import { FaShoppingCart } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+
+function Header() {
+  function handleNavClick() {
+    document.body.classList.remove("nav-open");
+  }
+  const activeStyles = {
+    fontWeight: "bold",
+    color: "#ffa506",
+  };
+
+  return (
+    <header className="header">
+      <button
+        class="nav-toggle"
+        aria-label="toggle navigation"
+        onClick={() => {
+          document.body.classList.toggle("nav-open");
+        }}
+      >
+        <span class="hamburger"></span>
+      </button>
+
+      <div className="site-logo">
+        <Link to="/">
+          <img src="http://localhost:5173/site-logo.png" alt="site-logo" />
+        </Link>
+      </div>
+
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+              onClick={handleNavClick}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/product"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+              onClick={handleNavClick}
+            >
+              Product
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/about"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+              onClick={handleNavClick}
+            >
+              About
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/contact"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+              onClick={handleNavClick}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+
+        <ul className="nav__list">
+          <li className="nav__item">
+            <NavLink
+              to="/cart"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+            >
+              <FaShoppingCart />
+            </NavLink>
+          </li>
+          <li className="nav__item">
+            <NavLink
+              to="/profile"
+              style={({ isActive }) => (isActive ? activeStyles : null)}
+              className="nav__link"
+            >
+              <CgProfile />
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
